de8628b7037065666a8c3b0ec88a359e961fcf6a
Andrew <cl_and@mail.ru>
2024-07-14 17:00:45 +0300

    add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
 import MainPage from './components/Main/Main.tsx';
 import {NextUIProvider} from "@nextui-org/react";
+import {Route, Routes, useNavigate} from 'react-router-dom';
+
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <NextUIProvider>
-      <MainPage />
+    <NextUIProvider navigate={navigate}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </NextUIProvider>
   )
 }
